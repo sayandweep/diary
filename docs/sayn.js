a2c0ef373a1c3e49da1edfa9a8327a79768ccfd1
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-07-30 15:19:13 +0530

    Update sayn.js

--- docs/sayn.js
+++ docs/sayn.js
@@ -90,15 +90,15 @@ anime.timeline({loop: true})
     delay: 1000
   });
 
-function clickCounter() {
+function clickCounter1() {
   if (typeof(Storage) !== "undefined") {
     if (localStorage.clickcount) {
       localStorage.clickcount = Number(localStorage.clickcount)+1;
     } else {
       localStorage.clickcount = 1;
     }
-    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    document.getElementById("result1").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
   } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    document.getElementById("result1").innerHTML = "Sorry, your browser does not support web storage...";
   }
 }
