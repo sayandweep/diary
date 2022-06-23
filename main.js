abbd891699c289b2923275a4fe5aeb9b3817ad18
sayandweep <81100142+sayandweep@users.noreply.github.com>
2022-06-23 19:42:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     sayandweep.style.background = "white";
   } else {
     sayandweep.style.background = "#FEFBF5";
   }
+};
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
